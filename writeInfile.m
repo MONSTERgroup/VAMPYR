478c4f0e6c17d0c41cbe247a218b1656c9b203ab
tori-miller <48074192+tori-miller@users.noreply.github.com>
2021-10-19 12:58:02 -0400

    Update writeInfile.m

--- writeInfile.m
+++ writeInfile.m
@@ -3,13 +3,18 @@ pname = 'C:\Users\victoria.miller\Documents\GitHub\Random-VPSC-MTEX-code-snippet
 fname_in = [pname filesep 'vpsc7.in'];
 
 
-%  
+%% Header info   
 infile = fopen(fname_in,'w');
-fprintf(infile,'%u                          number of elements (nelem)',nElement);%L1
-fprintf(infile,'%u                          number of phases (nph)',nPhase); %L2
-formatSpec = 
-fprintf(infile,'',); %L3
-%L4
+fprintf(infile,'%u                          number of elements (nelem)\n',nElement);%L1
+fprintf(infile,'%u                          number of phases (nph)\n',nPhase); %L2
+fprintf(infile,'%s', [num2str(phaseFrac, '%.2f') '                   relative vol. fract. of phases (wph(i))\n']); %L3
+
+%% Phase info 
+
+for i = 1:nPhase
+    
+    fprintf(infile,'%s', ['*INFORMATION ABOUT PHASE #' num2str(i) '\n']);%L4
+    fprintf(infile,'%.1f %.1f '); %L5 
 
 
-
+end
